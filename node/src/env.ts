8cd22e7a0a50414cde1ba8a90d157b63f809688f
guraiken <gustavo.fiabani@gmail.com>
2026-07-02 20:59:15 -0300

    fix: now the .env is at the root folder

--- node/src/env.ts
+++ node/src/env.ts
@@ -3,9 +3,9 @@ import 'dotenv/config'
 export const env = {
     chaveAcesso: process.env.CHAVE_ACESSO || "chaveSuperSecreta123456",
     chaveRefresh: process.env.CHAVE_REFRESH || "chaveSuperSecreta123456",
-    usuarioDB: process.env.USUARIO,
-    senhaDB: process.env.SENHA,
-    nomeBanco: process.env.NOME_BANCO,
+    usuarioDB: process.env.USUARIO || "postgres",
+    senhaDB: process.env.SENHA || "postgres",
+    nomeBanco: process.env.NOME_BANCO || "clinic",
     hostDB: process.env.DB_HOST || "localhost",
-    port: process.env.PORT
+    port: process.env.PORT || 5000
 }
