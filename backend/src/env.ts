f7825ea2e9d332483f14a75c8af6e232fee4ec4b
guraiken <gustavo.fiabani@gmail.com>
2026-07-09 12:38:26 -0300

    aws

--- backend/src/env.ts
+++ backend/src/env.ts
@@ -4,7 +4,7 @@ export const env = {
     chaveAcesso: process.env.CHAVE_ACESSO || "chaveSuperSecreta123456",
     chaveRefresh: process.env.CHAVE_REFRESH || "chaveSuperSecreta123456",
     usuarioDB: process.env.USUARIO || "postgres",
-    senhaDB: process.env.SENHA || "Milenium_123",
+    senhaDB: process.env.SENHA || "postgres",
     nomeBanco: process.env.NOME_BANCO || "clinic",
     hostDB: process.env.DB_HOST || "localhost",
     port: process.env.PORT || 5000
--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: `${import.meta.env.VITE_BASE_URL || 'http://localhost'}:5000`,
+  baseURL: `${import.meta.env.VITE_BASE_URL || '3.231.161.64'}:5000`,
 });
 
 apiClient.interceptors.request.use(
