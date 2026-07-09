db80785a21f04c79cbda1533e7c972d93051d819
guraiken <gustavo.fiabani@gmail.com>
2026-07-09 12:45:27 -0300

    fix: aws

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,23 +1,21 @@
 import axios from 'axios';
 
+const BASE_URL = import.meta.env.VITE_BASE_URL || 'http://3.231.161.64:5000';
+
 const apiClient = axios.create({
-  baseURL: `${import.meta.env.VITE_BASE_URL || '3.231.161.64'}:5000`,
+  baseURL: BASE_URL,
 });
 
 apiClient.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem('accessToken');
-
     if (token) {
       config.headers.set('Authorization', `Bearer ${token}`);
     }
-
     config.headers.set('Accept', 'application/json');
-
     return config;
   },
   (error) => Promise.reject(error)
 );
 
 export default apiClient;
-
